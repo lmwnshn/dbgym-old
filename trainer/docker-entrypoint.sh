f4e0c75a19c7a5f5045110907e4fbf4db91d34a3
Wan Shen Lim <wanshen.lim@gmail.com>
2023-03-16 08:05:19 +0800

    sudo?

--- trainer/docker-entrypoint.sh
+++ trainer/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-echo '/tmp/core.%h.%e.%t' > /proc/sys/kernel/core_pattern
+echo '/tmp/core.%h.%e.%t' | sudo tee /proc/sys/kernel/core_pattern
 ulimit -c unlimited
 
 sudo chown -R trainer:trainer /trainer
