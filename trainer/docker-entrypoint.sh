578a2e826cb12e37624a35837106d557f9958726
Wan Shen Lim <wanshen.lim@gmail.com>
2023-03-16 08:03:14 +0800

    Testing out core dumps on trainer.

--- trainer/docker-entrypoint.sh
+++ trainer/docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -euxo pipefail
 
+echo '/tmp/core.%h.%e.%t' > /proc/sys/kernel/core_pattern
+ulimit -c unlimited
+
 sudo chown -R trainer:trainer /trainer
 cd /app
 gunicorn "trainer.wsgi:app"
